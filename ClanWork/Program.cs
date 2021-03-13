56472a41bf7b5ebe149a56886259eb2460294692
Marcos Hernandez Grande <80291613+MarcosHndz2006@users.noreply.github.com>
2021-03-12 23:02:55 -0600

    First Program

--- ClanWork/Program.cs
+++ ClanWork/Program.cs
@@ -6,7 +6,20 @@ namespace ClanWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 0, b = 0;
+
+            Console.WriteLine("Ingrese un valor entero a 'a': "); //Se solicita al usuario que ingrese 
+            a = Convert.ToInt32(Console.ReadLine());              // un valor entero cualquiera para la variable 'a'
+            Console.WriteLine("Ingrese un valor entero a 'b': "); // y de igual manera para la variable 'b' desde teclado
+            b = Convert.ToInt32(Console.ReadLine());  
+
+            Console.WriteLine($"La suma de {a} + {b} da como resultado: {AddNumbers(a,b)}");
+
+        }
+
+        public static int AddNumbers(int a, int b)
+        {
+            return a+b;
         }
     }
 }
